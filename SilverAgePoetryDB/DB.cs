f8160339a3ed23555781db1b40e40bc30685cd24
Dunaeva <staceydunaeva@gmail.com>
2017-05-27 19:09:20 +0300

    чтение из файла

--- SilverAgePoetryDB/DB.cs
+++ SilverAgePoetryDB/DB.cs
@@ -15,14 +15,14 @@ namespace SilverAgePoetryDB
 
         static public bool AddPoem(Poem poem)
         {
-            int PoemKey = Poems.Count;
-            Poems.Add(poem);
-            XmlSerializer serializer = new XmlSerializer(typeof(Poem));
-            string PoemFilePath = "Poems\\$NAME$.xml";
-            PoemFilePath = PoemFilePath.Replace("$NAME$", Poems.IndexOf(poem).ToString());
-            File.Create("PoemFilePath");
             try
             {
+                int PoemKey = Poems.Count;
+                Poems.Add(poem);
+                XmlSerializer serializer = new XmlSerializer(typeof(Poem));
+                string PoemFilePath = "Poems\\$NAME$.xml";
+                PoemFilePath = PoemFilePath.Replace("$NAME$", Poems.IndexOf(poem).ToString());
+                File.Create("PoemFilePath");
                 TextWriter writer = new StreamWriter(PoemFilePath);
                 serializer.Serialize(writer, poem);
                 writer.Close();
@@ -36,14 +36,14 @@ namespace SilverAgePoetryDB
 
         static public bool AddAuthor(Author author)
         {
-            int AuthorKey = Authors.Count;
-            Authors.Add(author);
-            XmlSerializer serializer = new XmlSerializer(typeof(Author));
-            string AuthorFilePath = "Authors\\$NAME$.xml";
-            AuthorFilePath = AuthorFilePath.Replace("$NAME$", Authors.IndexOf(author).ToString());
-            File.Create("AuthorFilePath");
             try
             {
+                int AuthorKey = Authors.Count;
+                Authors.Add(author);
+                XmlSerializer serializer = new XmlSerializer(typeof(Author));
+                string AuthorFilePath = "Authors\\$NAME$.xml";
+                AuthorFilePath = AuthorFilePath.Replace("$NAME$", Authors.IndexOf(author).ToString());
+                File.Create("AuthorFilePath");
                 TextWriter writer = new StreamWriter(AuthorFilePath);
                 serializer.Serialize(writer, author);
                 writer.Close();
@@ -91,6 +91,50 @@ namespace SilverAgePoetryDB
                 {
                     return true;
                 }
+                return false;
+            }
+        }
+
+        static public bool ReadPoems()
+        {
+            try
+            {
+                string[] PoemFiles = Directory.GetFiles("Poems\\");
+                XmlSerializer serializer = new XmlSerializer(typeof(Poem));
+                FileStream fs;
+                Poem poem;
+                foreach (string poemName in PoemFiles)
+                {
+                    fs = new FileStream(poemName, FileMode.Open);
+                    poem = (Poem)serializer.Deserialize(fs);
+                    Poems.Add(poem);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        static public bool ReadAuthors()
+        {
+            try
+            {
+                string[] AuthorFiles = Directory.GetFiles("Authors\\");
+                XmlSerializer serializer = new XmlSerializer(typeof(Author));
+                FileStream fs;
+                Author author;
+                foreach (string authorName in AuthorFiles)
+                {
+                    fs = new FileStream(authorName, FileMode.Open);
+                    author = (Author)serializer.Deserialize(fs);
+                    Authors.Add(author);
+                }
+                return true;
+            }
+            catch
+            {
                 return false;
             }
         }
