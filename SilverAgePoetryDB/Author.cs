821fbd9f6ef9ac170348d424fcc3a3ff48db86c0
Dunaeva <staceydunaeva@gmail.com>
2017-05-27 15:11:14 +0300

    классы для авторов и произведений

---
+++ SilverAgePoetryDB/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilverAgePoetryDB
+{
+    class Author
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime DeathDate { get; set; }
+        public string BirthPlace { get; set; }
+        public string Biography { get; set; }
+        public Dictionary<int, Poem> Poems { get; set; }
+    }
+}
---
+++ SilverAgePoetryDB/Poem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilverAgePoetryDB
+{
+    class Poem
+    {
+        public string Name { get; set; }
+        public Author Author { get; set; }
+        public DateTime WritingStartDate { get; set; }
+        public DateTime WritingEndDate { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Text { get; set; }
+    }
+}
